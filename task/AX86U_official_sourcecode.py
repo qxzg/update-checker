84978442c6e31c037f35c544e39cc883d9ba60d0
qxzg <out@qxzg.xyz>
2020-11-07 18:41:20 +0800

    初步实现获取华硕官方固件信息号

---
+++ task/AX86U_official_sourcecode.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+Base_Url="https://www.asus.com.cn/support/api/product.asmx/GetPDDrivers?osid=8&website=cn&pdhashedid=pezdd5ujcut73gz5&model=RT-AX86U"
+
+def get_version():
+    req = requests.get(url=Base_Url)
+    req.raise_for_status() #检查返回值
+    req_date = req.json()
+    print(req_date['Result']['Obj'][1]['Name'])
+    print(req_date['Result']['Obj'][1]['Files'][0]['Version'])
+    print(req_date['Result']['Obj'][1]['Files'][0]['ReleaseDate'])
+    print(req_date['Result']['Obj'][1]['Files'][0]['DownloadUrl']['China'])
+    print(req_date['Result']['Obj'][1]['Files'][0]['Description'])
+    return
+
+get_version()
