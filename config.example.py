9ff60b6740e29ffe94f98d66cc76555b021a0c05
qxzg <out@qxzg.xyz>
2020-11-07 21:39:34 +0800

    数据库配置

---
+++ config.example.py
@@ -0,0 +1,9 @@
+
+#数据库配置
+DATABASE_CONFIG = {
+    'host'      : 'localhost',
+    'dbname'    : 'update_checker',
+    'username'  : 'update_checker',
+    'password'  : 'pwd',
+    'port'      : 3306
+}
