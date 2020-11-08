bfe9ee7dd7fcc3fe36600f657ae397ae5b8c37f7
qxzg <out@qxzg.xyz>
2020-11-08 19:06:35 +0800

    修改数据库结构，增加push表

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@
  Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 07/11/2020 20:35:36
+ Date: 08/11/2020 19:05:37
 */
 
 SET NAMES utf8mb4;
@@ -19,7 +19,21 @@ CREATE TABLE `config`  (
   `config_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `config_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `config_value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`config_id`) USING BTREE
+  PRIMARY KEY (`config_id`) USING BTREE,
+  UNIQUE INDEX `config_name`(`config_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for push
+-- ----------------------------
+DROP TABLE IF EXISTS `push`;
+CREATE TABLE `push`  (
+  `push_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '接收人ID',
+  `push_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '接收人名称',
+  `email_address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱地址',
+  `phone_number` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号',
+  `serverchan_key` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'ServerChande的KEY',
+  PRIMARY KEY (`push_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -29,12 +43,13 @@ DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task`  (
   `task_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '任务ID',
   `task_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '任务名',
-  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文件名',
   `enabled` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'no' COMMENT '启用状态',
   `task_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '任务状态',
   `last_run` datetime(6) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '任务最后运行时间',
-  `latest_version` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '最新版本号',
+  `latest_version` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '最新版本号',
   `release_date` datetime(6) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新日期',
+  `push_to` int(10) NOT NULL COMMENT '推送至ID',
   PRIMARY KEY (`task_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
