5b4064fad9dc812d910ee7abf0e34248ab2702c6
qxzg <out@qxzg.xyz>
2021-01-22 10:57:21 +0800

    update test

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 #import main
-import requests
-import time
+import main
 
-
-print(type(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+main.connect_db()
+print(main.push("sc", 1, "####  完美解码V.20201225 已发布  \n #### 下载地址： http://www.wmzhe.com/soft-13163.html"))
